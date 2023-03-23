a913d0e92ec99cb62dd715e510e3615e3435cdee
unknown <darshansri17@gmail.com>
2023-03-23 21:36:20 +0530

    first commit

---
+++ camunda-kafka-consumer/src/main/java/com/vso/camunda/CamundaKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.vso.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaKafkaConsumerApplication.class, args);
+	}
+
+}
